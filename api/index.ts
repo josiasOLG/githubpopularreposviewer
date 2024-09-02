7feaadfd4935932a6ac64a328aa3688eab82878d
josias <josiasoliveira111@gmail.com>
2024-09-02 17:25:02 -0300

    alterações

--- api/index.ts
+++ api/index.ts
@@ -43,10 +43,6 @@ app.use(bodyParser.json());
 app.use(
   cors({
     origin: ["*"],
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    allowedHeaders: ["Content-Type", "Authorization"],
-    exposedHeaders: ["access-token", "refresh-token"],
-    credentials: true, // Se você precisar enviar cookies junto com as requisições
   })
 );
 
