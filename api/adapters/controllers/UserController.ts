c823e774fecc68207384b4f3feb47699df51d47f
josias <josiasoliveira111@gmail.com>
2025-05-21 12:26:07 -0300

    fiz(): correção

--- api/adapters/controllers/UserController.ts
+++ api/adapters/controllers/UserController.ts
@@ -1,12 +1,11 @@
-import { Request, Response } from "express";
-import { CreateUser } from "../../usecases/user/CreateUser";
-import { GetUserById } from "../../usecases/user/GetUserById";
-import { UpdateUser } from "../../usecases/user/UpdateUser";
-import { DeleteUser } from "../../usecases/user/DeleteUser";
-import { UserRepository } from "../repositories/UserRepository";
-import { Router } from "express";
-import { appServiceRepository } from "../repositories/AppServiceRepository";
-import { AddressRepository } from "../repositories/AddressRepository";
+import { Request, Response, Router } from 'express';
+import { CreateUser } from '../../usecases/user/CreateUser';
+import { DeleteUser } from '../../usecases/user/DeleteUser';
+import { GetUserById } from '../../usecases/user/GetUserById';
+import { UpdateUser } from '../../usecases/user/UpdateUser';
+import { AddressRepository } from '../repositories/AddressRepository';
+import { appServiceRepository } from '../repositories/AppServiceRepository';
+import { UserRepository } from '../repositories/UserRepository';
 
 const userRepository = new UserRepository();
 const addressRepository = new AddressRepository();
@@ -19,7 +18,7 @@ export const createUser = async (req: Request, res: Response) => {
     const user = await createUser.execute(req.body);
     res.status(201).json(user);
   } catch (error) {
-    res.status(500).json({ error: "Falha ao criar usuário" });
+    res.status(500).json({ error: 'Falha ao criar usuário' });
   }
 };
 
@@ -30,15 +29,13 @@ export const getAgendaConfig = async (req: Request, res: Response) => {
     const agendaConfig = await userRepository.getAgendaConfig(userId);
 
     if (!agendaConfig) {
-      return res
-        .status(404)
-        .json({ error: "Agenda não configurada ou usuário não encontrado" });
+      return res.status(404).json({ error: 'Agenda não configurada ou usuário não encontrado' });
     }
 
     res.status(200).json({ agendaConfig });
   } catch (error) {
-    console.error("Erro ao obter agendaConfig:", error);
-    res.status(500).json({ error: "Erro ao obter agendaConfig" });
+    console.error('Erro ao obter agendaConfig:', error);
+    res.status(500).json({ error: 'Erro ao obter agendaConfig' });
   }
 };
 
@@ -47,21 +44,16 @@ export const getAgendaConfigService = async (req: Request, res: Response) => {
     const userId = req.params.id as string;
     const service = req.params.service as string;
 
-    const agendaConfig = await userRepository.getAgendaConfigService(
-      userId,
-      service
-    );
+    const agendaConfig = await userRepository.getAgendaConfigService(userId, service);
 
     if (!agendaConfig) {
-      return res
-        .status(404)
-        .json({ error: "Agenda não configurada ou usuário não encontrado" });
+      return res.status(404).json({ error: 'Agenda não configurada ou usuário não encontrado' });
     }
 
     res.status(200).json({ agendaConfig });
   } catch (error) {
-    console.error("Erro ao obter agendaConfig:", error);
-    res.status(500).json({ error: "Erro ao obter agendaConfig" });
+    console.error('Erro ao obter agendaConfig:', error);
+    res.status(500).json({ error: 'Erro ao obter agendaConfig' });
   }
 };
 
@@ -109,10 +101,10 @@ export const getUserById = async (req: Request, res: Response) => {
       };
       res.json(filteredUser);
     } else {
-      res.status(404).json({ error: "Usuário não encontrado" });
+      res.status(404).json({ error: 'Usuário não encontrado' });
     }
   } catch (error) {
-    res.status(500).json({ error: "Falha ao obter usuário" });
+    res.status(500).json({ error: 'Falha ao obter usuário' });
   }
 };
 
@@ -125,9 +117,8 @@ export const verifyUserById = async (req: Request, res: Response) => {
     if (user) {
       if (!user.endTime || !user.startTime) {
         return res.status(400).json({
-          error: "HORAS TRABALHADAS IMCOMPLETO",
-          message:
-            "Por favor, preencha todos os campos obrigatórios de horas trabalhadas.",
+          error: 'HORAS TRABALHADAS IMCOMPLETO',
+          message: 'Por favor, preencha todos os campos obrigatórios de horas trabalhadas.',
         });
       }
 
@@ -143,14 +134,14 @@ export const verifyUserById = async (req: Request, res: Response) => {
         !address.phoneNumber
       ) {
         return res.status(406).json({
-          error: "Dados de endereço incompletos",
+          error: 'Dados de endereço incompletos',
           message:
-            "Por favor, preencha todos os campos obrigatórios do endereço (rua, bairro, CEP, CPF e número de telefone).",
+            'Por favor, preencha todos os campos obrigatórios do endereço (rua, bairro, CEP, CPF e número de telefone).',
         });
       }
     }
   } catch (error) {
-    res.status(500).json({ error: "Falha ao obter usuário" });
+    res.status(500).json({ error: 'Falha ao obter usuário' });
   }
 };
 
@@ -160,12 +151,12 @@ export const updateUser = async (req: Request, res: Response) => {
     const updateUser = new UpdateUser(userRepository);
     const user = await updateUser.execute(userId, req.body);
     if (user) {
-      res.status(200).json({ mensagem: "Usuário atualizado com sucesso" });
+      res.status(200).json({ mensagem: 'Usuário atualizado com sucesso' });
     } else {
-      res.status(404).json({ error: "Usuário não encontrado" });
+      res.status(404).json({ error: 'Usuário não encontrado' });
     }
   } catch (error) {
-    res.status(500).json({ error: "Falha ao atualizar usuário" });
+    res.status(500).json({ error: 'Falha ao atualizar usuário' });
   }
 };
 
@@ -175,21 +166,18 @@ export const updateAgendaConfig = async (req: Request, res: Response) => {
     const { agendaConfig } = req.body;
 
     if (!agendaConfig) {
-      return res.status(400).json({ error: "agendaConfig é obrigatório" });
+      return res.status(400).json({ error: 'agendaConfig é obrigatório' });
     }
 
-    const updatedUser = await userRepository.updateAgendaConfig(
-      userId,
-      agendaConfig
-    );
+    const updatedUser = await userRepository.updateAgendaConfig(userId, agendaConfig);
 
     if (updatedUser) {
-      res.status(200).json({ mensagem: "Agenda atualizada com sucesso" });
+      res.status(200).json({ mensagem: 'Agenda atualizada com sucesso' });
     } else {
-      res.status(404).json({ error: "Usuário não encontrado" });
+      res.status(404).json({ error: 'Usuário não encontrado' });
     }
   } catch (error) {
-    res.status(500).json({ error: "Erro ao atualizar agenda" });
+    res.status(500).json({ error: 'Erro ao atualizar agenda' });
   }
 };
 
@@ -200,7 +188,7 @@ export const deleteUser = async (req: Request, res: Response) => {
     await deleteUser.execute(userId);
     res.sendStatus(204);
   } catch (error) {
-    res.status(500).json({ error: "Falha ao deletar usuário" });
+    res.status(500).json({ error: 'Falha ao deletar usuário' });
   }
 };
 
@@ -208,7 +196,7 @@ export const getBarbers = async (req: Request, res: Response) => {
   try {
     const barbers = await userRepository.getBarbers();
 
-    const filteredBarbers = barbers.map((barber) => ({
+    const filteredBarbers = barbers.map(barber => ({
       id: barber._id,
       name: barber.name,
       email: barber.email,
@@ -226,7 +214,7 @@ export const getBarbers = async (req: Request, res: Response) => {
 
     res.json(filteredBarbers);
   } catch (error) {
-    res.status(500).json({ error: "Falha ao obter barbeiros" });
+    res.status(500).json({ error: 'Falha ao obter barbeiros' });
   }
 };
 
@@ -251,10 +239,10 @@ export const updateService = async (req: Request, res: Response) => {
       };
       res.json(filteredUser);
     } else {
-      res.status(404).json({ error: "Usuário não encontrado" });
+      res.status(404).json({ error: 'Usuário não encontrado' });
     }
   } catch (error) {
-    res.status(500).json({ error: "Falha ao atualizar serviço" });
+    res.status(500).json({ error: 'Falha ao atualizar serviço' });
   }
 };
 
@@ -264,25 +252,23 @@ export const pesquisar = async (req: Request, res: Response) => {
     const { service } = req.params;
 
     if (!service) {
-      return res
-        .status(400)
-        .json({ error: "Parâmetros de consulta e serviço são necessários" });
+      return res.status(400).json({ error: 'Parâmetros de consulta e serviço são necessários' });
     }
 
     const appService = await appServiceRepository.findById(service);
 
     if (!appService || !appService.name) {
-      return res
-        .status(404)
-        .json({ error: "Serviço não encontrado ou não possui um nome" });
+      return res.status(404).json({ error: 'Serviço não encontrado ou não possui um nome' });
     }
 
-    const users = await userRepository.searchUsers(
-      query as string,
-      service as string,
+    const users = await userRepository.searchUsers(query as string | undefined, service);
+    console.log('users', users);
+    if (!users || users.length === 0) {
+      return res.status(404).json({ error: 'Nenhum usuário encontrado' });
+    }
+    const usersWithAgendaConfig = users.filter(
+      (user: any) => user.agendaConfig && Object.keys(user.agendaConfig.workDays).length > 0,
     );
-
-    const usersWithAgendaConfig = users.filter((user: any) => user.agendaConfig && Object.keys(user.agendaConfig.workDays).length > 0);
 
     const userNames = usersWithAgendaConfig.map((user: any) => ({
       id: user._id,
@@ -298,18 +284,20 @@ export const pesquisar = async (req: Request, res: Response) => {
       interval: user.interval,
       image: user.image,
       role: user.role,
-      address: {
-        street: user.address.street,
-        number: user.address.number,
-        complement: user.address.complement,
-        zipCode: user.address.zipCode,
-        city: user.address.city,
-        state: user.address.state,
-        country: user.address.country,
-        cpf: user.address.cpf,
-        phoneNumber: user.address.phoneNumber,
-        locality: user.address.locality,
-      },
+      address: user.address
+        ? {
+            street: user.address.street,
+            number: user.address.number,
+            complement: user.address.complement,
+            zipCode: user.address.zipCode,
+            city: user.address.city,
+            state: user.address.state,
+            country: user.address.country,
+            cpf: user.address.cpf,
+            phoneNumber: user.address.phoneNumber,
+            locality: user.address.locality,
+          }
+        : null,
     }));
 
     res.json({
@@ -320,8 +308,8 @@ export const pesquisar = async (req: Request, res: Response) => {
       users: userNames,
     });
   } catch (error) {
-    console.error("Falha ao pesquisar usuários:", error);
-    res.status(500).json({ error: "Falha ao pesquisar usuários" });
+    console.error('Falha ao pesquisar usuários:', error);
+    res.status(500).json({ error: 'Falha ao pesquisar usuários' });
   }
 };
 
@@ -338,10 +326,10 @@ export const getBarberHoursById = async (req: Request, res: Response) => {
         interval: user.interval,
       });
     } else {
-      res.status(404).json({ error: "Barbeiro não encontrado" });
+      res.status(404).json({ error: 'Barbeiro não encontrado' });
     }
   } catch (error) {
-    res.status(500).json({ error: "Falha ao obter horários do barbeiro" });
+    res.status(500).json({ error: 'Falha ao obter horários do barbeiro' });
   }
 };
 
@@ -354,24 +342,24 @@ export const getActive = async (req: Request, res: Response) => {
         active: user.active,
       });
     } else {
-      res.status(404).json({ error: "Barbeiro não encontrado" });
+      res.status(404).json({ error: 'Barbeiro não encontrado' });
     }
   } catch (error) {
-    res.status(500).json({ error: "Falha ao obter status do barbeiro" });
+    res.status(500).json({ error: 'Falha ao obter status do barbeiro' });
   }
 };
 
-router.post("/", createUser);
-router.get("/:id", getUserById);
-router.get("/verifyUserById/:id", verifyUserById);
-router.get("/:id/hours", getBarberHoursById); // Nova rota para obter horários de barbeiros
-router.get("/pesquisar/:service", pesquisar);
-router.put("/:id", updateUser);
-router.put("/:id/service", updateService);
-router.get("/:id/active", getActive);
-router.delete("/:id", deleteUser);
-router.put("/:id/agenda", updateAgendaConfig);
-router.get("/:id/agenda", getAgendaConfig);
-router.get("/:id/agenda/:service/service", getAgendaConfig);
+router.post('/', createUser);
+router.get('/:id', getUserById);
+router.get('/verifyUserById/:id', verifyUserById);
+router.get('/:id/hours', getBarberHoursById); // Nova rota para obter horários de barbeiros
+router.get('/pesquisar/:service', pesquisar);
+router.put('/:id', updateUser);
+router.put('/:id/service', updateService);
+router.get('/:id/active', getActive);
+router.delete('/:id', deleteUser);
+router.put('/:id/agenda', updateAgendaConfig);
+router.get('/:id/agenda', getAgendaConfig);
+router.get('/:id/agenda/:service/service', getAgendaConfig);
 
 export default router;
